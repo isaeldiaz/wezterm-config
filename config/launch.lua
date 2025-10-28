92fa2bf6ff8b872ba1386ffec7640948d0001761
Isael Diaz <Isael.Diaz@gmail.com>
2025-10-28 09:01:09 +0100

    inital fork from KevinSilvester/wezterm-config

--- config/launch.lua
+++ config/launch.lua
@@ -6,9 +6,9 @@ local options = {
 }
 
 if platform.is_win then
-   options.default_prog = { 'pwsh', '-NoLogo' }
+   options.default_prog = { 'powershell', '-NoLogo' }
    options.launch_menu = {
-      { label = 'PowerShell Core', args = { 'pwsh', '-NoLogo' } },
+      { label = 'PowerShell Core', args = { 'powershell', '-NoLogo' } },
       { label = 'PowerShell Desktop', args = { 'powershell' } },
       { label = 'Command Prompt', args = { 'cmd' } },
       { label = 'Nushell', args = { 'nu' } },
