72f7a2cf97fd6ce0f192b493430ca1325e1fc9c5
Isael Diaz <Isael.Diaz@numascale.com>
2026-04-28 15:04:09 +0200

    changing mac config to default to another font if JetBRainsMono not available

--- config/fonts.lua
+++ config/fonts.lua
@@ -7,11 +7,15 @@ local font_family = 'JetBrainsMono Nerd Font'
 
 local font_size = platform.is_mac and 12 or 9.75
 
+local font = platform.is_mac
+   and wezterm.font_with_fallback({
+      { family = font_family, weight = 'Medium' },
+      { family = 'Menlo' },
+   })
+   or wezterm.font({ family = font_family, weight = 'Medium' })
+
 return {
-   font = wezterm.font({
-      family = font_family,
-      weight = 'Medium',
-   }),
+   font = font,
    font_size = font_size,
 
    --ref: https://wezfurlong.org/wezterm/config/lua/config/freetype_pcf_long_family_names.html#why-doesnt-wezterm-use-the-distro-freetype-or-match-its-configuration
